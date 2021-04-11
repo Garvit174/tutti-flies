043e0a44ee8dff26f160708b3c146de2cde48df2
Derek <harrison.derek.w@gmail.com>
2021-04-11 15:29:52 +0200

    initial commit tutti game 2

---
+++ app/src/main/java/com/main/tuttigame2/GameView.java
@@ -0,0 +1,111 @@
+package com.main.tuttigame2;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+    private MainThread thread;
+    private CharacterSprite characterSprite;
+    public float loc_x;
+    public float loc_y;
+    public int action;
+    private Context context;
+
+    public GameView(Context context_main) {
+        super(context_main);
+
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(),this);
+
+        loc_x = 100;
+        loc_y = 100;
+        action = 3;
+
+        context = context_main;
+        setFocusable(true);
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                this.loc_x = (int) event.getX();
+                this.loc_y = (int) event.getY();
+                this.action = MotionEvent.ACTION_MOVE;
+                break;
+            case MotionEvent.ACTION_DOWN:
+                this.loc_x = (int) event.getX();
+                this.loc_y = (int) event.getY();
+                this.action = MotionEvent.ACTION_DOWN;
+                break;
+            case MotionEvent.ACTION_UP:
+                this.action = MotionEvent.ACTION_UP;
+                break;
+        }
+
+        return true;
+
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        characterSprite = new CharacterSprite(BitmapFactory.decodeResource(getResources(),R.drawable.tutti_copy_resized_2), context);
+
+        thread.setRunning(true);
+        thread.start();
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        retry = false;
+    }
+    }
+
+    public void update() {
+        characterSprite.update(loc_x, loc_y, action);
+
+    }
+
+    @Override
+    public void draw(Canvas canvas)
+    {
+
+        super.draw(canvas);
+        if(canvas!=null) {
+            characterSprite.draw(canvas);
+
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/main/tuttigame2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.main.tuttigame2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(new GameView(this));
+
+    }
+}
